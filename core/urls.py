a29c5210eb102785d8df8eb0ad1b85e4fdc05cd1
erickjfreitas <erickstar10@gmail.com>
2023-07-12 10:17:00 -0300

    commit

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, cadastre
 
 urlpatterns = [
     path('', views.home),
     path('login/', LoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name="logout")
+   
 
 
 ]
--- core/views.py
+++ core/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request,'space/pages/home.html')
 
+
 # Create your views here.
