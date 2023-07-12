4741e1c58cd0a1c3a7d391dc8d572e20e74d7eb9
LuisP30 <pimenta.lh2@gmail.com>
2023-07-12 10:15:15 -0300

    commit

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
+app_name= 'core'
+
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('resultados', views.resultados, name='resultados'),
+
+    
     path('login/', LoginView.as_view(), name='login'),
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", LogoutView.as_view(), name="logout"),
 
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+
 
 def home(request):
     return render(request,'space/pages/home.html')
 
-# Create your views here.
+def resultados(request, id):
+    veiculo = Veiculo.objects.filter(is_published = True).order_by('-id')
+    return render(request, 'space/pages/resultados.html', context={
+        'veiculo' : veiculo,
+
+    })
