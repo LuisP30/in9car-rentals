e35b19cd9a671ce2b4b50024ee85c027ad5df0ac
LuisP30 <pimenta.lh2@gmail.com>
2023-07-26 16:48:12 -0300

    commit

--- core/models.py
+++ core/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 class Veiculo(models.Model):
-    class cambioVeiculo(models.TextChoices):
+
+    class CambioVeiculo(models.TextChoices):
         MANUAL = 'M',
         AUTOMATICO = 'A',
+
+    class TipoVeiculo(models.TextChoices):
+        CARRO = "C",
+        MOTO = "M",
 
     modelo = models.CharField(max_length=45)
     marca = models.CharField(max_length=15)
@@ -15,7 +20,13 @@ class Veiculo(models.Model):
     potencia = models.CharField(max_length=45)
     cor = models.CharField(max_length=12)
     ano = models.CharField(max_length=4)
+    ar_condicionado = models.BooleanField(null=False, default=True)
+    CD_DVD = models.BooleanField(null=False, default=True)
     disponivel = models.BooleanField(default=False)
     valor = models.DecimalField(max_digits=5, decimal_places=2)
-    cambio = models.CharField(max_length=1, choices=cambioVeiculo.choices)
+    cambio = models.CharField(max_length=1, choices=CambioVeiculo.choices) # noqa
+    tipo = models.CharField(max_length=1, choices=TipoVeiculo.choices, null=False) # noqa
     cover = models.ImageField(upload_to='core/covers/%Y/%m/%d/', blank=False)
+
+    def __str__(self):
+        return self.marca + ' ' + self.modelo
--- core/views.py
+++ core/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Veiculo
 
 
 def home(request):
-    return render(request,'space/pages/home.html')
+    return render(request, 'space/pages/home.html')
+
 
 def resultados(request):
-    return render(request,'space/pages/resultados.html')
+    veiculos = Veiculo.objects.filter(disponivel=True).order_by('-id').get()
+    return render(request, 'space/pages/resultados.html', context={
+        'veiculos': veiculos,
+    })
+
 
 def detalhes(request):
-    return render(request,'space/pages/detalhes.html')
+    return render(request, 'space/pages/detalhes.html')
